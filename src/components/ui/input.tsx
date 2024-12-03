dd5c5d545ebf6a21dc9cd3189ea3d88845fa68ef
chanwukim <chnwu.kim@gmail.com>
2024-12-03 18:40:45 +0900

    feat: Label 컴포넌트
    fix: input focus ring 색 수정

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -19,7 +19,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           <input
             type={type}
             className={cn(
-              "file:text-foreground placeholder:text-foreground-muted flex h-36 w-full rounded-md border bg-transparent px-12 py-4 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              "file:text-foreground placeholder:text-foreground-muted focus-visible:ring-primary flex h-36 w-full rounded-md border bg-transparent px-12 py-4 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
               LeftIcon && "pl-32",
               className,
             )}
